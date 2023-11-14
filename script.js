e8a4c614709e2ca8b9823aa33bcfadd28c1e7bb8
idrisselbergui <you@example.com>
2023-11-14 16:23:50 +0100

    school

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 var pass_input=document.getElementById("pass_form");
+var new_montant=document.getElementById("new_montant");
 var error= document.getElementById("error");
-onload();
-function onload(){
+var montant_pass=[["123456",1000],["0000",4500]]
+var montant;
+
+
+function validation(){
+
+    if(check_pass(pass_input.textContent)){
+    document.getElementById("pass_form").innerHTML="";
     document.getElementById("pagePassword").style.display="none";
+    document.getElementById("pagemontant").style.display="block";
+    document.getElementById("mont").innerHTML=montant;
+    }else{
+        error.innerHTML=pass_input.textContent;
+        pass_input.value="";
+        pass_input.textContent="";
+    }
+ }
+
+function check_pass(pass){
+        if(montant_pass[0][0]===pass){
+            montant=montant_pass[0][1];
+            console.log(montant)
+            return true;
+        }
+        else if(montant_pass[1][0]===pass){
+            montant=montant_pass[1][1];
+            console.log(montant)
+            return true;
+        }
+    
 }
 function Show(num){
    if(pass_input.textContent.length<=6){
@@ -21,10 +49,28 @@ function display(){
 
 function check_etat(){
   var checkbox= document.getElementById("check")
-   if(checkbox.checked==true){
+    if(checkbox.checked==true){
     pass_input.type='text';
+    }
+    else{
+        pass_input.type='password'
+    }
 }
-   else{
-    pass_input.type='password'
-}
-}
+
+function autre_montant(num){
+    new_montant.textContent+=num;
+    new_montant.value=new_montant.textContent;
+ }
+ function autre_montant_button(){
+    document.getElementById("pagemontant").style.display="none";
+    document.getElementById("pageautremontant").style.display="block";
+
+ }
+
+
+ function validation_montant(){
+
+ }
+ function montant(montant){
+
+ }
